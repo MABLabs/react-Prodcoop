79c0f27a7f62ce1bfbc889d9f8ca13b1ff5574d4
Micheal <micheal.beaudoin@gmail.com>
2017-11-06 23:22:18 -0600

    added ri=un.sh script on server

--- public/version.js
+++ public/version.js
@@ -1,8 +1,8 @@
 
 var version = {
 		"name":      "react-Prodcoop",
-		"buildTime": "14:19:59.437Z",
-		"buildDate": "2017-05-24",
+		"buildTime": "05:06:10.304Z",
+		"buildDate": "2017-11-07",
 		"version":   "1.0.0",
 		"NodeJS":    "v7.7.1",
 		"PkgReact":     "^15.4.4",
---
+++ server/run.sh
@@ -0,0 +1,2 @@
+sudo node serverClient.js 80
+
