323713d187205f16ac13e1a97688364a07cfb132
michael beaudoin <michael.beaudoin@mablabs.com>
2018-06-03 07:23:26 -0500

    Cleaned up react bundle and modified entry css

--- public/version.js
+++ public/version.js
@@ -1,8 +1,8 @@
 
 var version = {
 		"name":      "react-Prodcoop",
-		"buildTime": "19:58:44.563Z",
-		"buildDate": "2018-06-01",
+		"buildTime": "11:49:39.214Z",
+		"buildDate": "2018-06-03",
 		"version":   "1.0.0",
 		"NodeJS":    "v7.7.1",
 		"PkgReact":     "^15.4.4",
